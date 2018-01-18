bc00d55fc349e922124c521674589dc732636f2f
Chris Gullickson <cgullickson5@gmail.com>
2018-01-18 18:58:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     if @user = nil
-      session[:user_id] = @user_id
+      session[:user_id] = @user.id
       redirect '/account'
     else
       erb :error
